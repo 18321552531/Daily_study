af7f3161150233ec7c595e1cc809402430343010
18321552531 <729269746@qq.com>
2019-08-18 21:36:50 +0800

    20190818

---
+++ py_pycharm/Flask_learning/11.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019-08-18  00:52
+# @File    : 11.py
+# @Author  : 啊啊
+
+from flask import Flask, redirect, url_for, request
+app = Flask(__name__)
+
+@app.route('/success/<name>')
+def success(name):
+   return 'welcome %s' % name
+
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      user = request.form['nm']
+      return redirect(url_for('success',name = user))
+   else:
+      user = request.args.get('nm')
+      return redirect(url_for('success',name = user))
+
+if __name__ == '__main__':
+   app.run(debug = True)
+
+
+
