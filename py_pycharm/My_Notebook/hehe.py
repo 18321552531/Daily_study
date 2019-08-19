6275dcdcd156c5205bf3d163e60335fd4cce4bad
18321552531 <729269746@qq.com>
2019-08-19 22:02:13 +0800

    20190819

---
+++ py_pycharm/My_Notebook/hehe.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019-08-19  14:21
+# @File    : hehe.py
+# @Author  : 啊啊
+
+
+
+
--- py_pycharm/small_project/cai_bird3.0/2.基本数据类型.py
+++ py_pycharm/small_project/cai_bird3.0/2.基本数据类型.py
@@ -3,6 +3,13 @@
 # @File    : 2.基本数据类型.py
 # @Author  : 啊啊
 
-
+import os
+list = []
+filename = os.listdir('/Users/zhushengzu/python_load/py_pycharm/My_Notebook')
+for i in filename:
+    if i.split('.')[-1] == 'todo':
+        list.append(i)
+print(list)
+print(filename)
 
 
