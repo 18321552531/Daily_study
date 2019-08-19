033b5c07e968476b49d8b8ec65e43dde9cb70929
18321552531 <729269746@qq.com>
2019-08-20 00:00:59 +0800

    练习

---
+++ MySql_test/Sql_test.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019-08-19  23:25
+# @File    : Sql_test.py
+# @Author  : 啊啊
+
+import pymysql
+
+config = {
+    'host': '127.0.0.1',
+    'port': 3306,
+    'user': 'root',
+    'passwd': 'zszaa0805',
+    'db': 'Mysql_test',
+}
+name = 'admin'
+db = pymysql.connect(**config)
+cursor = db.cursor()
+cursor.execute(r"select * from signin_table where username like '%s' " %name)
+res = cursor.fetchall()
+print(res[0][2])
+# print(res.values())
+if res == ():
+    print('ok')
+# if res[0]:
+#     print('ok')
+print(res)
+
+
+
